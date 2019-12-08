c0826b356ec778fad16479422d6bfc6e803485bc
joevaske <vasicdjordje@hotmail.com>
2019-12-08 14:30:40 +0100

    Verzija 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
 
  function myFunction(x) {
-    x.classList.toggle("change");
+    //x.classList.toggle("change");
    // document.getElementById("menu-items").classList.remove("invisible");
    
  
-    var menu1 = document.getElementById('menu-items');
-  
-
-     menu1.classList.toggle("invisible");
     
    
-
+    
 
   }
 
 
-  $( ".menu-bar-left" ).click(function() {
-    $( ".menu" ).slideToggle( "slow", function() {
-      // Animation complete.
+
+  $(document).ready(function(){
+
+      
+   var  menuBar = $("#menu-bar");
+   var menuItems = $("#menu-items");
+   
+   menuBar.click(function(){
+      menuItems.toggleClass("invisible");
+      menuBar.toggleClass("change");
+      
+      //menuItems.slideToggle("invisible");
+     
     });
+    
   });
